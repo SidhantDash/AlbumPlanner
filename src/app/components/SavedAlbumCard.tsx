da90985cd3485fdc6360d6f8e18b94e7f51c0468
SidhantDash <sidhantdash126@gmail.com>
2025-08-31 01:45:57 -0400

    Semi-working drag and drop function in SavedAlbums.tsx

--- src/app/components/SavedAlbumCard.tsx
+++ src/app/components/SavedAlbumCard.tsx
@@ -1,9 +1,11 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import Image from "next/image";
 import { useState } from "react";
+import { Draggable } from "@hello-pangea/dnd";
 
 interface SavedAlbumProps {
     id: string;
+    index: number;
     title: string;
     artists: [
         {
@@ -16,7 +18,7 @@ interface SavedAlbumProps {
     removeSavedAlbum: any;
 }
 
-export default function SavedAlbumCard({id, title, artists, cover, releaseDate, dateAdded, removeSavedAlbum} : SavedAlbumProps) {
+export default function SavedAlbumCard({id, index, title, artists, cover, releaseDate, dateAdded, removeSavedAlbum} : SavedAlbumProps) {
     const totalArtists = artists.length;
     let artistString = "";
     artists.map((artists, index) => {
@@ -32,40 +34,53 @@ export default function SavedAlbumCard({id, title, artists, cover, releaseDate, 
     }
 
     return (
-        <div className="w-full h-28 bg-mq-lightgray rounded-3xl flex flex-row items-center justify-between  overflow-hidden shrink-0">
-            <div className="w-1/7 h-full flex justify-center items-center transition-all hover:border-2 hover:bg-[#454545] active:transition-none active:border-3 active:border-mq-lightblue cursor-grab rounded-l-3xl saved_albums_hamburger_icon">
-                <GiHamburgerMenu />
-            </div>
-            <div className="w-5/7 h-3/4 px-1 flex flex-row gap-x-2 grow justify-start items-center">
-                <div className="h-full aspect-square rounded-2xl overflow-hidden shrink-0 select-none">
-                    <Image
-                        src={cover}
-                        alt="No Album Cover Provided"
-                        width={300}
-                        height={300}
-                        className="w-full h-full object-cover rounded-2xl"
-                    />
-                </div>
-                <div className="flex flex-col h-full justify-start">
-                    <h1 className="font-bold text-sm text-ellipsis line-clamp-1">{title}</h1>
-                    <h2 className="font-semibold text-sm text-ellipsis line-clamp-1 text-mq-lightblue">{artistString}</h2>
-                    <h2 className="font-medium text-sm">{releaseDate}</h2>
-                    <h2 className="font-normal text-xs line-clamp-1 text-[#B7B7B7] italic">
-                        Added by: {dateAdded}
-                    </h2>
-                </div>
-            </div>
-            <label className="w-1/7 h-full flex justify-center items-center transition-all hover:border-2 hover:bg-[#454545] hover:has-checked:border-3 hover:has-checked:border-mq-lightblue cursor-pointer rounded-r-3xl">
-                <input
-                    type="checkbox"
-                    className="cursor-pointer w-6 h-6 appearance-none border-2 border-white rounded bg-transparent checked:hover:bg-mq-lightblue checked:bg-mq-lightblue checked:border-mq-lightblue transition-colors"
-                    checked={isSelected}
-                    onChange={() => handleOnChange()}
+        <Draggable
+            draggableId={id}
+            index={index}
+        >
+            {(provided) => (
+                <div
+                    className="w-full h-28 bg-mq-lightgray rounded-3xl flex flex-row items-center justify-between  overflow-hidden shrink-0"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
                 >
-                </input>
-            </label>
-
-        </div>
+                    <div
+                        className="w-1/7 h-full flex justify-center items-center transition-all hover:border-2 hover:bg-[#454545] active:transition-none active:border-3 active:border-mq-lightblue cursor-grab rounded-l-3xl saved_albums_hamburger_icon"
+                        {...provided.dragHandleProps}
+                    >
+                        <GiHamburgerMenu />
+                    </div>
+                    <div className="w-5/7 h-3/4 px-1 flex flex-row gap-x-2 grow justify-start items-center">
+                        <div className="h-full aspect-square rounded-2xl overflow-hidden shrink-0 select-none">
+                            <Image
+                                src={cover}
+                                alt="No Album Cover Provided"
+                                width={300}
+                                height={300}
+                                className="w-full h-full object-cover rounded-2xl"
+                            />
+                        </div>
+                        <div className="flex flex-col h-full justify-start">
+                            <h1 className="font-bold text-sm text-ellipsis line-clamp-1">{title}</h1>
+                            <h2 className="font-semibold text-sm text-ellipsis line-clamp-1 text-mq-lightblue">{artistString}</h2>
+                            <h2 className="font-medium text-sm">{releaseDate}</h2>
+                            <h2 className="font-normal text-xs line-clamp-1 text-[#B7B7B7] italic">
+                                Added by: {dateAdded}
+                            </h2>
+                        </div>
+                    </div>
+                    <label className="w-1/7 h-full flex justify-center items-center transition-all hover:border-2 hover:bg-[#454545] hover:has-checked:border-3 hover:has-checked:border-mq-lightblue cursor-pointer rounded-r-3xl">
+                        <input
+                            type="checkbox"
+                            className="cursor-pointer w-6 h-6 appearance-none border-2 border-white rounded bg-transparent checked:hover:bg-mq-lightblue checked:bg-mq-lightblue checked:border-mq-lightblue transition-colors"
+                            checked={isSelected}
+                            onChange={() => handleOnChange()}
+                        >
+                        </input>
+                    </label>
+                </div>
+            )}
+        </Draggable>
     );
 
 }
--- src/app/components/SavedAlbums.tsx
+++ src/app/components/SavedAlbums.tsx
@@ -1,35 +1,73 @@
 import SavedAlbumCard from "./SavedAlbumCard";
 import { DragDropContext } from "@hello-pangea/dnd";
+import { Droppable } from "@hello-pangea/dnd";
 
 interface SavedAlbumsProps {
     savedAlbums: SavedAlbum[];
     removeSavedAlbum: any;
 }
 
+interface Results {
+    draggableId: string;
+    sourceId: string;
+}
+
 export default function SavedAlbums({savedAlbums, removeSavedAlbum}: SavedAlbumsProps) {
+
+    function onDragEnd(sourceIndex: string, draggableId: string, destinationIndex?: string, ) {
+
+        if (!destinationIndex) return;
+
+        if (sourceIndex === destinationIndex) { // See if location of draggable changes
+            return; // Means we don't need to do anything
+        }
+
+        // const savedAlbumsColumn = state.columns["SavedAlbumsDroppable"];
+        // const newSavedAlbumIds = Array.from(savedAlbumsColumn)
+        
+        return "thing";
+
+    };
+    
+
     return (
-        <div className="bg-mq-darkgray h-full max-h-full rounded-3xl p-8 flex flex-col gap-y-4 overflow-y-scroll">
-            {savedAlbums.length === 0 ? (
-                <div className="h-full w-full flex justify-center items-center text-xl italic text-[#B7B7B7]">
-                    No Saved Albums
-                </div>
-            ) : (savedAlbums.map( (album: SavedAlbum) => {
-                
-                console.log(album.images);
-                return (
-                    <SavedAlbumCard 
-                        key={album.id}
-                        id={album.id}
-                        title={album.name}
-                        artists={album.artists}
-                        releaseDate={album.release_date}
-                        cover={album.images[0].url ?? "/images/EmptySymbol.png"}
-                        dateAdded={album.date_added}
-                        removeSavedAlbum={removeSavedAlbum}
-                    />
-                );
-            }))}
-            
-        </div>
+        <DragDropContext
+            onDragEnd={onDragEnd}
+        >
+
+            <Droppable
+                droppableId={"SavedAlbumsDroppable"}
+            >
+                {provided => (
+                    <div
+                        className="bg-mq-darkgray h-full max-h-full rounded-3xl p-8 flex flex-col gap-y-4 overflow-y-scroll"
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                    >
+                        {savedAlbums.length === 0 ? (
+                            <div className="h-full w-full flex justify-center items-center text-xl italic text-[#B7B7B7]">
+                                No Saved Albums
+                            </div>
+                        ) : (savedAlbums.map( (album: SavedAlbum, index: number) => {
+                            return (
+                    
+                                    <SavedAlbumCard
+                                        key={album.id}
+                                        id={album.id}
+                                        index={index}
+                                        title={album.name}
+                                        artists={album.artists}
+                                        releaseDate={album.release_date}
+                                        cover={album.images[0].url ?? "/images/EmptySymbol.png"}
+                                        dateAdded={album.date_added}
+                                        removeSavedAlbum={removeSavedAlbum}
+                                    />
+                            );
+                        }))}
+                    
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
     );
 }
